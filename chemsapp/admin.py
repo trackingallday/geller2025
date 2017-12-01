d69ccdc0a522b4f367b1da798db171204b5fec59
fllfl <rimu@fisherieslogistics.com>
2017-12-01 15:33:31 +1300

    add the admin site

--- chemsapp/admin.py
+++ chemsapp/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from chemsapp.models import SafetyWear, Distributor, Customer, User, Profile,\
+    Product, ProductAdd, ProductRemove
 
-# Register your models here.
+
+class SafetyWearAdmin(admin.ModelAdmin):
+    pass
+
+
+class DistributorAdmin(admin.ModelAdmin):
+    pass
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAddAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductRemoveAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(SafetyWear, SafetyWearAdmin)
+admin.site.register(Distributor, DistributorAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductAdd, ProductAddAdmin)
+admin.site.register(ProductRemove, ProductRemoveAdmin)
--- chemsapp/models.py
+++ chemsapp/models.py
@@ -2,25 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class MyBaseModel(models.model):
+class MyBaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     archived = models.BooleanField(default=False)
 
 
-class Profile(models.Model, MyBaseModel):
+class Profile(MyBaseModel):
     user = models.OneToOneField(User)
     phone_number = models.CharField(max_length=100)
     cell_phone_number = models.CharField(max_length=100)
     business_name = models.CharField(max_length=255)
-    lat = models.DecimalField()
-    lon = models.DecimalField()
+    lat = models.DecimalField(max_digits=80, decimal_places=8)
+    lon = models.DecimalField(max_digits=80, decimal_places=8)
 
     def __str__(self):
         return '%s %s %s'.format(self.business_name, self.user.first_name, self.user.last_name)
 
 
-class Product(models.Model, MyBaseModel):
+class Product(MyBaseModel):
     name = models.CharField(max_length=255, unique=True)
     primaryImageLink = models.CharField(max_length=500)
     secondaryImageLink = models.CharField(max_length=500)
@@ -45,8 +45,8 @@ class Distributor(Profile):
     customers = models.ManyToManyField(Customer, related_name="distributors")
 
 
-class ProductAdd(models.Model, MyBaseModel):
-    product = models.ForeignKey(Product, related_name="productsAdds", on_delete=models.CASCADE)
+class ProductAdd(MyBaseModel):
+    productAdded = models.ForeignKey(Product, related_name="productsAdds", on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, related_name="productAdds", on_delete=models.CASCADE)
     distributor = models.ForeignKey(Distributor, related_name="productAdds", on_delete=models.CASCADE)
 
@@ -54,8 +54,8 @@ class ProductAdd(models.Model, MyBaseModel):
         return '%s %s %s'.format(self.distributor.business_name, self.customer.business_name, self.product.name)
 
 
-class ProductRemove(models.Model, MyBaseModel):
-    product = models.ForeignKey(Product, related_name="productsRemoves", on_delete=models.CASCADE)
+class ProductRemove(MyBaseModel):
+    productRemoved = models.ForeignKey(Product, related_name="productsRemoves", on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, related_name="productRemoves", on_delete=models.CASCADE)
     distributor = models.ForeignKey(Distributor, related_name="productRemoves", on_delete=models.CASCADE)
 
@@ -63,9 +63,10 @@ class ProductRemove(models.Model, MyBaseModel):
         return '%s %s %s'.format(self.distributor.business_name, self.customer.business_name, self.product.name)
 
 
-class SafetyWear(models.Model, MyBaseModel):
+class SafetyWear(MyBaseModel):
     name = models.CharField(max_length=255)
     imageLink = models.CharField(max_length=455)
+
     def __str__(self):
         return self.name
 
