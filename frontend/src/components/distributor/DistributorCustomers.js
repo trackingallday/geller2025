296d31956bc141e99c03712c4f499dc89f49c442
fllfl <rimu@fisherieslogistics.com>
2017-12-09 10:44:47 +1300

    cleanup

--- frontend/src/components/distributor/DistributorCustomers.js
+++ frontend/src/components/distributor/DistributorCustomers.js
@@ -4,10 +4,6 @@ import axios from 'axios';
 import serverUrl from '../../constants/serverUrl';
 import CustomersTable from './CustomersTable';
 
-const data = [{"phoneNumber":"235235325",
-  "first_name":"aliens","last_name":"inspace","email":"aliens@space.ship",
-  "cellPhoneNumber":"2345234235","businessName":"b name","lat":"23.00000000","lon":"23.00000000",
-  "products":["SpraynWipe - avondale chemical bros and company","SpraynWipe2 - smo bros"]}];
 
 export default class DistributorCustomers extends Component {
 
@@ -23,7 +19,6 @@ export default class DistributorCustomers extends Component {
 
   componentDidMount() {
     axios.defaults.headers.common['Authorization'] = this.props.token;
-    console.log(axios.defaults.headers);
     axios.get(serverUrl + '/customers_list/')
       .then((response) => {
         const customers = response.data.map((c, i) => {
