a488d2a98f8d88f70e17f0bcc9bd9eedfe6afa79
fllfl <rimu@fisherieslogistics.com>
2017-12-10 17:26:48 +1300

    add the ajax search function

---
+++ frontend/src/util/SearchMapbox.js
@@ -0,0 +1,17 @@
+import xhr from 'xhr';
+
+
+const accessToken = 'pk.eyJ1IjoiY2hlbXNhcHAiLCJhIjoiY2pheWdzNjd6MTJnbDMzczczbG92ZG15ayJ9.sqO7Za5EYpuhj6REKyneFA';
+const endpoint = 'http://api.mapbox.com';
+const source = 'mapbox.places';
+
+export default function search(query='', callback) {
+  const searchTime = new Date();
+  const uri = `${endpoint}/geocoding/v5/${source}/${encodeURIComponent(query)}.json?access_token=${accessToken}&country=NZ`;
+  xhr({
+    uri: uri,
+    json: true
+  }, (err, res, body) => {
+    callback(err, res, body, searchTime);
+  });
+}
