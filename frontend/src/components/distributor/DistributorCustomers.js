2dd75cdb6bce15c0d38f8d3d69adbf8afa975e74
fllfl <rimu@fisherieslogistics.com>
2017-12-10 17:28:25 +1300

    create the forms differently outside of the render - so can add props and performance

--- frontend/src/components/distributor/DistributorCustomers.js
+++ frontend/src/components/distributor/DistributorCustomers.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import { Modal, Button, Row, Col  } from 'antd';
-import axios from 'axios';
-import serverUrl from '../../constants/serverUrl';
+import { Modal, Button, notification, Row, Col, Form  } from 'antd';
 import CustomersTable from './CustomersTable';
+import NewCustomerForm from '../common/NewCustomerForm';
+import EditCustomerForm from '../common/EditCustomerForm';
+import { getCustomers } from '../../util/DjangoApi';
+
+
+const openNotification = ({ message, description }) => {
+  const args = {
+    message,
+    description,
+    duration: 6,
+  };
+  notification.open(args);
+};
 
 
 export default class DistributorCustomers extends Component {
@@ -14,44 +25,107 @@ export default class DistributorCustomers extends Component {
       customersData: [],
       searchText: '',
       filtered: false,
+      showNew: null,
+      showEdit: null,
     };
   }
 
   componentDidMount() {
-    axios.defaults.headers.common['Authorization'] = this.props.token;
-    axios.get(serverUrl + '/customers_list/')
-      .then((response) => {
-        const customers = response.data.map((c, i) => {
-          return Object.assign(c, c.user, { key: i });
-        });
-        console.log(customers)
-        this.setState({
-          customersData: customers,
-        });
-      }).catch((error) => {
-        console.log(error);
+    this.getCustomersData();
+    this.NewCustomerForm = Form.create()(NewCustomerForm);
+    this.MyEditCustomerForm = Form.create()(EditCustomerForm);
+  }
+
+  getCustomersData = () => {
+    getCustomers( (customers) => {
+      this.setState({
+        customersData: customers,
       });
+    });
   }
 
   onMenuClick = ({ item, key, keyPath }) => {
     this.setState({ selectedMenuItem: key })
   }
 
+  toggleNew = () => {
+    if(this.state.show) {
+      this.props.form.resetFields();
+    }
+    this.setState({
+      showNew: this.state.showNew ? null : true,
+    });
+  }
+
+  toggleEdit = (value, record) => {
+    if(!this.state.showEdit) {
+      this.MyEditCustomerForm = Form.create(record)(EditCustomerForm);
+    }
+    this.setState({
+      showEdit: this.state.showEdit ? null : true,
+      customerToEdit: record,
+    });
+  }
+
+  onNewCustomer = (response) => {
+    this.toggleNew();
+    this.getCustomersData();
+    openNotification({ message: response.data.message,
+      description: 'Nice work you have successfully added a customer'});
+  }
+
+  onEditCustomer = (response) => {
+    this.toggleEdit();
+    this.getCustomersData();
+    openNotification({ message: response.data.message,
+      description: 'Wonderful work you have successfully edited the customer'});
+  }
+
   render = () => {
+
+    const { state, toggleNew, toggleEdit, onNewCustomer, onEditComplete } = this;
+    const { showNew, showEdit, customersData, customerToEdit } = state;
+
+    const formProps = {
+      onCreate: onNewCustomer,
+      onEditComplete: this.onNewCustomer,
+      customersData,
+      customerToEdit,
+    };
+
+    const MyNewCustomerForm = this.MyNewCustomerForm;
+    const MyEditCustomerForm = this.MyEditCustomerForm;
+
     return (
       <div>
         <Row type="flex" justify="end" style={{ paddingBottom: 12}}>
           <Col span={21}>
           </Col>
           <Col span={3}>
-            <Button>New Customer</Button>
+            <Button onClick={toggleNew}>New Customer</Button>
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-            <CustomersTable data={this.state.customersData} />
+            <CustomersTable data={state.customersData} onEditClick={this.toggleEdit} />
           </Col>
         </Row>
+        <Modal
+          title="Create new customer"
+          visible={showNew}
+          footer={null}
+          onCancel={toggleNew}
+        >
+          { showNew && <MyNewCustomerForm { ...formProps }/> }
+        </Modal>
+        <Modal
+          title="Edit customer"
+          visible={showEdit}
+          footer={null}
+          onCancel={toggleEdit}
+        >
+          { showEdit && <MyEditCustomerForm { ...formProps } /> }
+        </Modal>
       </div>
     );
   }
