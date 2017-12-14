1b89e9953a1b65e6dafb3ad56e5c5d96c8ec2bcb
fllfl <rimu@fisherieslogistics.com>
2017-12-14 17:42:33 +1300

    fix warnings

--- frontend/src/util/Sorter.js
+++ frontend/src/util/Sorter.js
@@ -5,7 +5,7 @@ const prepString = (str) => {
 export function alphabetSort(a, b) {
   const aa = prepString(a);
   const bb = prepString(b);
-  if(aa == bb) {
+  if(aa === bb) {
     return 0;
   }
   return aa < bb ? -1 : 1
