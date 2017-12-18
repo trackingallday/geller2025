9b10c02be950e430fe499a9009b0ba1fbc0210da
fllfl <rimu@fisherieslogistics.com>
2017-12-19 10:16:52 +1300

    add map serialisers and qr code files

--- chemsapp/serializers.py
+++ chemsapp/serializers.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.models import User
-from chemsapp.models import ProductRemove, ProductAdd, Product, Customer, Profile, SafetyWear
+from chemsapp.models import ProductRemove, Distributor, ProductAdd, Product, Customer, Profile, SafetyWear
 from rest_framework import serializers
+import pyqrcode
+import base64
+import io
+import chemicaldatasheets.settings
+
+
+
+def path_to_qr_code(path):
+    base_url = 'http://localhost:8000'
+    base_data_url = 'data:image/png;base64,'
+    link = base_url + path.url
+    url = pyqrcode.create(link.upper())
+    buffer = io.BytesIO()
+    url.png(buffer)
+    encoded_string = base64.b64encode(buffer.getvalue()).decode()
+    return base_data_url + encoded_string
+
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -44,6 +61,8 @@ class CustomerSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
 
     editable = serializers.SerializerMethodField('get_can_edit')
+    sdsQrcode = serializers.SerializerMethodField('sds_qrcode')
+
     customers = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
@@ -52,7 +71,7 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'name', 'primaryImageLink', 'secondaryImageLink', 'usageType', 'amountDesc',
             'instructions', 'productCode', 'brand', 'infoSheet', 'sdsSheet',
-            'safetyWears', 'customers', 'editable',
+            'safetyWears', 'customers', 'editable', 'sdsQrcode',
         )
 
     def get_can_edit(self, obj):
@@ -69,6 +88,59 @@ class ProductSerializer(serializers.ModelSerializer):
             print(serializer.data)
             return serializer.data
         return []
+
+    def sds_qrcode(self, obj):
+        return path_to_qr_code(obj.sdsSheet)
+
+
+class DistributorSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer(many=False, read_only=True)
+    customers = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Distributor
+        fields = (
+            'id', 'phoneNumber', 'user', 'cellPhoneNumber', 'businessName',
+            'customers', 'address', 'geocodingDetail',
+        )
+
+
+    distributors = DistributorSerializer(many=True, read_only=True)
+    products = serializers.StringRelatedField(many=True, read_only=True)
+    productIds = serializers.SerializerMethodField('product_ids')
+
+    class Meta:
+        model = Customer
+        read_only_fields = ('distributors', 'products')
+        fields = (
+            'geocodingDetail',
+            'id',
+            'businessName',
+            'products',
+            'distributors',
+            'products',
+            'productIds',
+        )
+
+
+    def product_ids(self, obj):
+        return [p.id for p in obj.products.all()]
+
+
+class ProductMapSerializer(serializers.ModelSerializer):
+
+    customers = CustomerMapSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        read_only_fields = ('customers', )
+        fields = (
+            'id', 'name', 'brand', 'customers',
+        )
+
+
+
 
 
 class SafetyWearSerializer(serializers.ModelSerializer):
