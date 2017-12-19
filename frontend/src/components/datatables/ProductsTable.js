efb9fe211253481f06142a4d4d729a2c40cf0b1f
fllfl <rimu@fisherieslogistics.com>
2017-12-19 14:33:45 +1300

    change image url

--- frontend/src/components/datatables/ProductsTable.js
+++ frontend/src/components/datatables/ProductsTable.js
@@ -5,7 +5,8 @@ import { alphabetSort } from '../../util/Sorter';
 
 
 const { Meta } = Card;
-const url = 'http://127.0.0.1:8000';
+//const url = 'http://127.0.0.1:8000';
+const url = 'http://104.236.249.246';
 
 const productDetailList = [
   [ 'Name', 'name'],
