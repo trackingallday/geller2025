39004b51f7af968882cfb7814530be383ed384cc
fllfl <rimu@fisherieslogistics.com>
2017-12-23 13:04:02 +1300

    better url constant

--- frontend/src/constants/serverUrl.js
+++ frontend/src/constants/serverUrl.js
@@ -1,3 +1,12 @@
-//export default "";
-//export default "http://127.0.0.1:8000";
-export default "";
+const dev = true;
+const DEV_URL = '0.0.0.0:8000';
+const PRODUCTION_URL = '104.236.249.246:8000';
+let url = PRODUCTION_URL;
+
+if(dev) {
+  url = DEV_URL;
+}
+
+console.log(DEV_URL, PRODUCTION_URL);
+
+export default `http://${url}`
