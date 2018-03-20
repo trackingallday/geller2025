b1997de05435abcf32ff17be43c574e873baa19d
fllfl <rimu@fisherieslogistics.com>
2018-03-21 00:40:25 +1300

    add marketing front end

--- marketing_front/src/components/Post.js
+++ marketing_front/src/components/Post.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router'
-import URI from '../../constants/serverUrl';
+import URI from '../constants/serverUrl';
 //const URI = 'http://localhost:8000';
 
 class Post extends Component {
