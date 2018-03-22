4f26c8fedf185e588fceadeb141147fbb1956f44
fllfl <rimu@fisherieslogistics.com>
2018-03-22 20:01:02 +1300

    no refresh page

--- marketing_front/src/App.js
+++ marketing_front/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     reqeust.get(URI + '/public_products/')
       .set('accept', 'json')
       .end((err, res) => {
-        this.setState({data: JSON.parse(res.text)});
+        this.setState({data: JSON.parse(res.text), loaded: true});
       });
   }
 
@@ -113,6 +113,7 @@ class App extends Component {
           </div>
           <Footer />
         </div>
+        { !this.state.loaded && <div style={{position: 'absolute', height: '100%', width: '100%', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: '#fff', zIndex: 9999}} /> }
       </div>);
   }
 }
