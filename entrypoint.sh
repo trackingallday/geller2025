e82a0a99d2f0837a2897ac3400ae4377ae47059d
trackingallday <rimu.boddy@sitem8.com>
2025-04-25 23:18:11 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "making migrations..."
 python manage.py makemigrations --noinput
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+python manage.py migrate --noinput --fake-initial
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
